06791436779ab9e60aa82f43ca878641b082c0ca
JuanAVillagomez <angelvillagomez25@gmail.com>
2026-01-10 21:03:08 -0600

    Add files via upload

---
+++ ESP32-Smart-Presence-Lamp/src/secrets_example.h
@@ -0,0 +1,20 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+// WiFi Credentials
+#define WIFI_SSID "WiFi_Name"
+#define WIFI_PASS "WiFi_Password"
+
+// Adafruit IO
+#define AIO_USERNAME "AIO_Username"
+#define AIO_KEY      "AIO_Key"
+
+// OpenWeatherMap
+#define WEATHER_API_KEY "OWM_Key"
+#define WEATHER_CITY    "City"
+#define WEATHER_COUNTRY "Country"
+
+// Secrets
+#define PRIVATE_PULSE_CODE "Private_Code"
+
+#endif
